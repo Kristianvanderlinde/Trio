f2303665450e90c389b230fb43d6570bf6e8c594
Kristian van der Linde <kristian.vanderlinde@broekhuis.nl>
2025-02-02 00:08:16 +0100

    fixed 'Unexpected end of file at 156:1 in StatRootView.swift' error

--- FreeAPS/Sources/Modules/Stat/View/StatRootView.swift
+++ FreeAPS/Sources/Modules/Stat/View/StatRootView.swift
@@ -8,17 +8,17 @@ extension Stat {
     struct RootView: BaseView {
         let resolver: Resolver
         @StateObject var state = StateModel()
-
+        
         @FetchRequest(
             entity: TDD.entity(),
             sortDescriptors: [NSSortDescriptor(key: "timestamp", ascending: false)]
         ) var fetchedTDD: FetchedResults<TDD>
-
+        
         @FetchRequest(
             entity: InsulinDistribution.entity(),
             sortDescriptors: [NSSortDescriptor(key: "date", ascending: false)]
         ) var fetchedInsulin: FetchedResults<InsulinDistribution>
-
+        
         enum Duration: String, CaseIterable, Identifiable {
             case Today
             case Day
@@ -27,14 +27,14 @@ extension Stat {
             case Total
             var id: Self { self }
         }
-
+        
         @State private var selectedDuration: Duration = .Today
         @State var paddingAmount: CGFloat? = 10
         @State var headline: Color = .secondary
         @State var days: Double = 0
         @State var pointSize: CGFloat = 3
         @State var conversionFactor = 0.0555
-
+        
         @ViewBuilder func stats() -> some View {
             ZStack {
                 let filter = DateFilter()
@@ -82,7 +82,7 @@ extension Stat {
                 }
             }
         }
-
+        
         @ViewBuilder func chart() -> some View {
             let filter = DateFilter()
             switch selectedDuration {
@@ -133,7 +133,7 @@ extension Stat {
                 )
             }
         }
-
+        
         var body: some View {
             HStack {
                 VStack(alignment: .center, spacing: 16) {
@@ -143,13 +143,14 @@ extension Stat {
                             Text(NSLocalizedString(duration.rawValue, comment: "")).tag(Optional(duration))
                         }
                     }
-                .pickerStyle(.segmented)
-                stats()
+                    .pickerStyle(.segmented)
+                    stats()
+                }
+                .onAppear(perform: configureView)
+                .navigationBarTitle("Statistics")
+                .navigationBarTitleDisplayMode(.automatic)
+                .navigationBarItems(leading: Button("Close", action: state.hideModal))
             }
-            .onAppear(perform: configureView)
-            .navigationBarTitle("Statistics")
-            .navigationBarTitleDisplayMode(.automatic)
-            .navigationBarItems(leading: Button("Close", action: state.hideModal))
         }
     }
 }
